e6b42452b7c775d8a758f2c5ca6c0396bd8de7d8
wldnz <wildanofficial32@gmail.com>
2025-05-30 06:44:21 +0700

    change to fix some mistake

--- app/Views/admin/DetailAkun.php
+++ app/Views/admin/DetailAkun.php
@@ -44,8 +44,8 @@
                         <label for="role">Role: </label>
                         <select name="role" id="role">
                             <?php  
-                                $account = session()->get("account");
-                                if($account["role"] == "admin"){ ?>
+                                $accountSesi = session()->get("account");
+                                if($accountSesi["role"] == "admin"){ ?>
                                     <option <?= $account["role"] == "staff" ? "value='staff' selected" : "value='staff'" ?>>Pegawai</option>
                                     <option <?= $account["role"] == "admin" ? "value='admin' selected" : "value='admin'" ?>>Admin</option>
                                     <?php } ?> 
