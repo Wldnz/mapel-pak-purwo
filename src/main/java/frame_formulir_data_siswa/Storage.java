22572d546ef56ac6a548b8ce41f9167dfacb45a8
wldnz <wildanofficial32@gmail.com>
2025-07-25 06:46:52 +0700

    first commit, btw ini membangun aplikasi berbasi desktop untuk formulir siswa ya adik'

---
+++ src/main/java/frame_formulir_data_siswa/Storage.java
@@ -0,0 +1,128 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package frame_formulir_data_siswa;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Wildan
+ */
+public class Storage {
+    
+    private final HashMap<String,String> storage = new HashMap<>();
+    private String valueText = "";
+    
+    public Storage(){
+         try{
+        
+            File file = new File("storage.txt");
+            if(!file.exists()){
+                try {
+                    file.createNewFile();
+                } catch (IOException ex) {
+                    Logger.getLogger(Storage.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            Scanner scanner = new Scanner(file);
+
+            int columnIndex = 0;
+            String[] columnName = {"nama", "umur", "jenis_kelamin", "minat_jurusan"};
+            String[] dataSiswa = {};
+            while(scanner.hasNextLine()){
+                
+                if(columnIndex == 3){
+                    columnIndex = 0;
+                    this.setStorage(dataSiswa);
+                    dataSiswa = {};
+                }   
+                columnIndex++;
+                dataSiswa[columnIndex] = scanner.nextLine();
+//                this.setStorage(data[0],data[1]);
+            }
+            
+            scanner.close();
+            
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+    
+    
+    private void setStorage(String key, String value){
+        String[] columnName= {"name", "umur", "jenis_kelamin", "minat_jurusan"};
+        this.storage.put(key, value);
+    }
+    
+    public void WriteData(String[] data) {
+        FileWriter file = null;
+        String[] columnName= {"name", "umur", "jenis_kelamin", "minat_jurusan"};
+        try {
+            file = new FileWriter("storage.txt");
+            for(int i=0; i < columnName.length; i++){
+                valueText = columnName[i] + "=" + data[i] + "\n";
+            }
+            storage.forEach((k, v) -> {
+                valueText += k + "=" + v + "\n";
+            }); 
+            file.write(valueText);
+            file.close();
+            this.storage.put(key, value);
+        } catch (IOException ex) {
+            Logger.getLogger(Storage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void removeData(String key){
+        FileWriter file = null;
+        try {
+            file = new FileWriter("storage.txt");
+            valueText = "";
+            storage.forEach((k, v) -> {
+                if(!k.equals(k)){
+                    valueText += k + "=" + v + "\n";
+                }
+            }); 
+            file.write(valueText);
+            file.close();
+            this.storage.remove(key);
+        } catch (IOException ex) {
+            Logger.getLogger(Storage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+    public HashMap<String,String> getAll(){
+        return this.storage;
+    }
+    
+    public String getDataByKey(String key){
+        return this.storage.get(key);
+    }
+    
+    public void clear(){
+        FileWriter file = null;
+        try {
+            file = new FileWriter("secret/storage.txt");
+            valueText = "";
+            file.write(valueText);
+            file.close();
+            this.storage.clear();
+        } catch (IOException ex) {
+            Logger.getLogger(Storage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.storage.clear();
+    }
+    
+    
+}
